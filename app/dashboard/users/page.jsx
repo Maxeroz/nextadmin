60df62d9ee819842139637338f241c2b2b321e03
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-10-23 00:29:58 +0400

    Single User and Product Page Design

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -47,7 +47,7 @@ function Users() {
             <td>active</td>
             <td>
               <div className={styles.buttons}>
-                <Link href="/">
+                <Link href="/dashboard/users/test">
                   <button className={`${styles.button} ${styles.view}`}>
                     View
                   </button>
