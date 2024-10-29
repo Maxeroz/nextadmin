9e72ea3c2c9d5c41eebe1c8d857ca998d6b21914
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-10-30 02:27:06 +0400

    Server Actions

---
+++ app/lib/actions.js
@@ -0,0 +1,61 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { Product, User } from "./models";
+import { connectToDB } from "./utils";
+import { redirect } from "next/navigation";
+import bcrypt from "bcrypt";
+
+export const addUser = async (formData) => {
+  const { username, password, email, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+
+    const salt = await bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      email,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    });
+
+    await newUser.save();
+  } catch (error) {
+    throw new Error("Failed to create user!");
+  }
+
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
+};
+
+export const addProduct = async (formData) => {
+  const { title, price, stock, desc, color, size } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+
+    const newProduct = new Product({
+      title,
+      price,
+      stock,
+      desc,
+      color,
+      size,
+    });
+
+    await newProduct.save();
+  } catch (error) {
+    throw new Error("Failed to add product!");
+  }
+
+  revalidatePath("/dashboard/products");
+  redirect("/dashboard/products");
+};
